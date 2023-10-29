c89af4f22b5e57f8ae0712285d628786507d3344
Alexandre Emygdio <emygdio.alex@gmail.com>
2023-10-29 17:38:46 -0300

    a

--- TesteConsole/TesteConsole/Program.cs
+++ TesteConsole/TesteConsole/Program.cs
@@ -5,13 +5,7 @@ namespace TesteConsole
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-
-            //
-            // 
-
-            Console.WriteLine("");
+        {        
 
 
         }
