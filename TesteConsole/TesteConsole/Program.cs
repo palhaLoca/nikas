c421d4356e1f4eadf29eee380c1614f0aee6577e
Alexandre Emygdio <emygdio.alex@gmail.com>
2023-10-29 16:41:02 -0300

    primeiro commit

---
+++ TesteConsole/TesteConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TesteConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // teste
+        }
+    }
+}
